75762c6c05e4d83de7bac4360c177c936c646517
Riccardo Giomi <me.personally.myself@gmail.com>
2025-10-30 13:21:04 +0100

    Refactor Cv model, add tests.

--- app/models/cv.rb
+++ app/models/cv.rb
@@ -8,28 +8,24 @@ class Cv < ApplicationRecord
   # Creates and associates an empty Contact at the end of the #contacts
   # association.
   def add_contact
-    max_position = contacts.maximum(:position) || 0
-    contacts << Contact.new(contact_type: "generic", position: max_position + 1)
-    self
+    max_position = contacts.map(&:position).max || 0
+    Contact.new(contact_type: "generic", position: max_position + 1).tap { |record| contacts << record }
   end
 
   def delete_contact(contact)
     id = contact.respond_to?(:id) ? contact.id : contact.to_i
     Contact.delete(id)
-    self
   end
 
   # Creates and associates an empty EducationItem at the end of the
   # #education_items association.
   def add_education_item
-    max_position = education_items.maximum(:position) || 0
-    education_items << EducationItem.new(position: max_position + 1)
-    self
+    max_position = education_items.map(&:position).max || 0
+    EducationItem.new(position: max_position + 1).tap { |record| education_items << record }
   end
 
   def delete_education_item(education_item)
     id = education_item.respond_to?(:id) ? education_item.id : education_item.to_i
     EducationItem.delete(id)
-    self
   end
 end
--- test/models/cv_test.rb
+++ test/models/cv_test.rb
@@ -6,4 +6,52 @@ class CvTest < ActiveSupport::TestCase
 
     assert cv.valid?
   end
+
+  test '#add_contact adds a new empty Contact with updated position and type "generic"' do
+    contact = nil
+    cv = Cv.new
+
+    assert_difference("cv.contacts.size", 1) { contact = cv.add_contact }
+
+    assert contact.value.blank?
+    assert_equal "generic", contact.contact_type
+    assert_equal 1, contact.position
+
+    assert_difference("cv.contacts.size", 1) { contact = cv.add_contact }
+    assert_equal 2, contact.position
+  end
+
+  test "#delete_contact destroys a Contact record but does not update the other positions" do
+    cv = cvs(:two)
+    first_id = cv.contacts.first.id
+    second_position = cv.contacts.second.position
+
+    assert_difference("cv.contacts.count", -1) { cv.delete_contact(first_id) }
+    assert_equal second_position, cv.contacts.reload.first.position
+    assert_difference("cv.contacts.count", -1) { cv.delete_contact(cv.contacts.first) }
+  end
+
+  test "#add_education_item adds a new empty Education_Item with updated position" do
+    education_item = nil
+    cv = Cv.new
+
+    assert_difference("cv.education_items.size", 1) { education_item = cv.add_education_item }
+    assert education_item.name.blank?
+    assert education_item.location.blank?
+    assert education_item.date.blank?
+    assert_equal 1, education_item.position
+
+    assert_difference("cv.education_items.size", 1) { education_item = cv.add_education_item }
+    assert_equal 2, education_item.position
+  end
+
+  test "#delete_education_item destroys a Education_Item record but does not update the other positions" do
+    cv = cvs(:two)
+    first_id = cv.education_items.first.id
+    second_position = cv.education_items.second.position
+
+    assert_difference("cv.education_items.count", -1) { cv.delete_education_item(first_id) }
+    assert_equal second_position, cv.education_items.reload.first.position
+    assert_difference("cv.education_items.count", -1) { cv.delete_education_item(cv.education_items.first) }
+  end
 end
