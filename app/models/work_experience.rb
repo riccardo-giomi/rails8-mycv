52e3a1ba714b1c63cdcc6d0b521828fc4d72d0cd
Riccardo Giomi <me.personally.myself@gmail.com>
2025-10-31 11:19:57 +0100

    Show pills preview for work-experience tags.

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,3 +1,6 @@
 class WorkExperience < ApplicationRecord
   belongs_to :cv
+
+  def has_tags? = tags.present?
+  def tags_array = tags&.strip&.split(",")&.map(&:strip) || []
 end
--- test/models/work_experience_test.rb
+++ test/models/work_experience_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class WorkExperienceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#has_tags?" do
+    without_tags = WorkExperience.new
+    with_tags = WorkExperience.new(tags: "tag1, tag2")
+    with_annoying_tags = WorkExperience.new(tags: "    ")
+
+    refute without_tags.has_tags?
+    refute with_annoying_tags.has_tags?
+    assert with_tags.has_tags?
+  end
+
+  test "#tags_array returns an array from the comma separated strings in #tags" do
+    no_tags = WorkExperience.new
+    no_real_tags = WorkExperience.new(tags: "    ")
+    with_tags = WorkExperience.new(tags: " tag1 ,tag2, tag3 ")
+
+    assert_equal [], no_tags.tags_array
+    assert_equal [], no_real_tags.tags_array
+
+    assert_equal %w[tag1 tag2 tag3], with_tags.tags_array
+  end
 end
