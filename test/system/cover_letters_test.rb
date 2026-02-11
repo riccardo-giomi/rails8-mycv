de3fb672cce7f837a4274c042ebc82e4202dcd9f
Riccardo Giomi <me.personally.myself@gmail.com>
2026-02-11 11:26:39 +0100

    Basic system test for cover_letters#index.

---
+++ test/system/cover_letters_test.rb
@@ -0,0 +1,28 @@
+require "application_system_test_case"
+
+class CoverLettersTest < ApplicationSystemTestCase
+  setup do
+    @cl_full = cover_letters(:one)
+    @cl_minimal = cover_letters(:two)
+  end
+
+  test "visiting the index" do
+    visit cover_letters_url
+
+    assert_selector "h1", text: "Cover Letters"
+
+    # turbo-frame
+    within("#cover_letter_#{@cl_full.id}") do
+      assert_selector "p", text: @cl_full.name
+      assert_selector "p" do
+        assert_link @cl_full.company_name, href: @cl_full.company_url
+      end
+    end
+
+    # turbo-frame
+    within("#cover_letter_#{@cl_minimal.id}") do
+      assert_selector "p", text: @cl_minimal.name
+      assert_selector "p", text: @cl_minimal.company_name
+    end
+  end
+end
